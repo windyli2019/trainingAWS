f378fedf1d3510a1ebd0760e45c0245378730e82
windyli2019 <lidalei2015@gmail.com>
2021-12-26 12:55:22 +0900

    add an comment

--- dbaccess.php
+++ dbaccess.php
@@ -37,6 +37,7 @@ function escape1($str)
 <title>test page for database access</title>
 </head>
 <body >
+Add Comment by Windyli!!<br<br>
 Last Access Time<br><br>
 <?php foreach ($rec as $a):?>
         <?=escape1($a)?><br>
